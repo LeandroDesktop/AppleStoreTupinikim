dce0700b6d516f60cc3be2715c656b2c7160cf3c
LeandroMonkey <leantech2021@gmail.com>
2023-06-08 13:46:28 -0300

    Primeira Versao

---
+++ AppStoreTupinikim/AppStoreTupinikim/Controllers/Produto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppStoreTupinikim.Controllers
+{
+    public class Produto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
